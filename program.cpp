cc7101c92922f3e397593331d7e3b618a08c5305
Kuba <quba560@gmail.com>
2017-05-29 15:34:27 +0200

    fixed var names, added basic thread variables

--- program.cpp
+++ program.cpp
@@ -1,20 +1,27 @@
+//
+// Project for SO2
+// JWM
+//
+
 #include <iostream>
 #include <thread>
 #include <mutex>
 #include <fstream>
 #include <queue>
 
-
 using namespace std;
-queue <long long> LICZBY_LOSOWE;
+
+queue <long long> LICZBY_LOSOWE; // kolejka przechowujaca liczby losowe
+const int THREAD_COUNT = 4; // ilosc watkow
+bool THREAD_STOP = false;
 
 void generuj(){
   long long random_value = 0; //deklaracja losowej liczby
     size_t size = sizeof(random_value); //rozmiar danych
     ifstream urandom("/dev/urandom", ios::in|ios::binary);
-    if(urandom) //if open true
+    if(urandom)
     {
-      for(int i=0;i<10;i++){
+      while(!THREAD_STOP){
         urandom.read(reinterpret_cast<char*>(&random_value), size); //read urandom
         if(urandom) //read ok
         {
@@ -35,9 +42,9 @@ void generuj(){
 };
 
 int main(){
-  auto generatorThread = thread(generuj);
+  auto GENERATOR = thread(generuj);
   cout<<LICZBY_LOSOWE.size()<<endl;
-  generatorThread.join();
+  GENERATOR.join();
   cout<<LICZBY_LOSOWE.size()<<endl;
 
   return 0;
